995cf856d21e44e66ccd7fd79231d80977a14fd0
Daniel Frey <dann.frey@me.com>
2018-10-31 18:03:43 -0700

    signup / login working

---
+++ front-end/src/components/dashboard/dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Dashboard extends React.Component {
+  render() {
+    return (
+        <p>You are logged in, sucka!</p>
+    );
+  }
+}
+
+export default Dashboard;
